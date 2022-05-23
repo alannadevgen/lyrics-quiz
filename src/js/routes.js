c40fea4b7ff1b6b2f1145eb7854fc53d8e2b0e02
alannagenin <alannagenin@gmail.com>
2022-05-23 14:07:15 +0200

    Routes : ajout path /manche

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,15 @@
 
 import HomePage from '../pages/home.jsx';
+import ManchePage from '../pages/manche.jsx';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
+  },
+  {
+    path: '/manche',
+    component: ManchePage,
   },
 ];
 
