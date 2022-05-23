e6bd29188ad857132549c7d0dea353a1cc5e4139
alannagenin <alannagenin@gmail.com>
2022-05-23 11:47:29 +0200

    Update store avec nouvelles variables manche, score et nom d'équipe

--- src/js/store.js
+++ src/js/store.js
@@ -3,17 +3,35 @@ import { createStore } from 'framework7/lite';
 
 const store = createStore({
   state: {
-    words: []
+    words: [],
+    manche,
+    scores: [],
+    nomEquipes: []
   },
   getters: {
     words({ state }) {
       return state.words;
+    },
+    manche({ state }) {
+      return state.manche;
+    },
+    scores({ state }) {
+      return state.scores;
+    },
+    nomEquipes({ state }) {
+      return state.nomEquipes;
     }
   },
   actions: {
     loadWords({ state }, path) {
-      state.words = fetch(path).then(response => response.json()).then(words => state.words = words);;
+      state.words = fetch(path).then(response => response.json()).then(words => state.words = words);
     },
+    incrementScore({ state }, numEquipe){
+      state.scores[numEquipe] += 1;
+    },
+    incrementManche({ state }, numManche){
+      state.manche[numManche] += 1;
+    }
   },
 })
 export default store;
